bb19034f3415aec42a301459c36098838b61e342
RohitShedage <rohitshedage@gmail.com>
2016-06-04 00:14:20 +0530

    updated mongo url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ path = require('path'),
 fs = require('fs'),
 cake = require('./app/server/Cake');
 
-mongoose.connect('mongodb://root:9SSyro8I4bHx@ec2-54-165-229-42.compute-1.amazonaws.com:27017/admin');
+mongoose.connect('mongodb://chatak:chatak@ec2-54-165-229-42.compute-1.amazonaws.com:27017/admin');
 //mongoose.connect('mongodb://localhost:27017/cakes');
 
 var storage =   multer.diskStorage({
